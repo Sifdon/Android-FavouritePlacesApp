c754882066ed2a81cb7ab403c9950e21f69de7f7
Alicja Suszwedyk <asuszwedyk@gmail.com>
2018-01-24 00:44:41 +0100

    add markers and populate geofence list

--- app/src/main/java/com/example/alicja/favouriteplacesapp/PlacesActivity.java
+++ app/src/main/java/com/example/alicja/favouriteplacesapp/PlacesActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
@@ -22,11 +23,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,26 +60,42 @@ public class PlacesActivity extends FragmentActivity implements OnMapReadyCallba
     // FIREBASE
     private DatabaseReference databaseReference;
     private FirebaseDatabase firebaseDatabase;
-    private ChildEventListener childEventListener;
 
     // LIST WITH FAVOURITE LOCATIONS FOR GEOFENCE
     private List<com.example.alicja.favouriteplacesapp.Location> favouritePlacesList;
+
+    // GEOFENCE
+    private ArrayList<Geofence> mGeofenceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
 
-        // Getting list of favourite places passed from MainActivity
-        favouritePlacesList = new ArrayList<>();
-        Intent intent = getIntent();
-        if(intent.getExtras() != null) {
-            Bundle bundle = intent.getExtras();
-            if(bundle.getParcelableArrayList("favouritePlaces") != null) {
-                favouritePlacesList = bundle.getParcelableArrayList("favouritePlaces");
-                Log.d(TAG, "onCreate: " + favouritePlacesList.size());
+        //setting up firebase database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Places");
+
+        //add listener to respond to changes in firebase DB with favourite locations and update list
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                favouritePlacesList = new ArrayList<>();
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+                    com.example.alicja.favouriteplacesapp.Location location = noteDataSnapshot.getValue(com.example.alicja.favouriteplacesapp.Location.class);
+                    favouritePlacesList.add(location);
+                }
+
+                Log.d(TAG, "onDataChange: " + favouritePlacesList.size());
             }
-        }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.e(TAG, "onCancelled: ", error.toException());
+            }
+        });
+
+        mGeofenceList = new ArrayList<>();
 
 
         // FAB for adding new favourite place
@@ -97,23 +120,79 @@ public class PlacesActivity extends FragmentActivity implements OnMapReadyCallba
 
         // Start location updates
         startLocationUpdates();
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if(checkPermission()) {
+        if (checkPermission()) {
             googleMap.setMyLocationEnabled(true);
         }
         // Get last location
         getLastKnownLocation();
+
+
+        if (favouritePlacesList != null) {
+            // Add geofences to all locations from favouritePlacesList
+            addGeofencesToFavouriteLocations();
+            // Draw markers
+            addMarkersToFavouriteLocations();
+        }
     }
 
+    private void addGeofencesToFavouriteLocations() {
+        for (com.example.alicja.favouriteplacesapp.Location location : favouritePlacesList) {
+
+            mGeofenceList.add(new Geofence.Builder()
+                    // Set the request ID of the geofence. This is a string to identify this
+                    // geofence.
+                    .setRequestId(location.getId())
+
+                    // Set the circular region of this geofence.
+                    .setCircularRegion(
+                            location.getLatitude(),
+                            location.getLongitude(),
+                            location.getRadius()
+                    )
+
+                    // Set the expiration duration of the geofence. This geofence gets automatically
+                    // removed after this period of time.
+                    .setExpirationDuration(12 * 60 * 60 * 1000)
+
+                    // Set the transition types of interest. Alerts are only generated for these
+                    // transition. We track entry and exit transitions in this sample.
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+
+                    // Create the geofence.
+                    .build());
+
+            Log.d(TAG, "addGeofencesToFavouriteLocations: geofence added for location " + location.getId());
+        }
+    }
+
+    private void addMarkersToFavouriteLocations() {
+        for (com.example.alicja.favouriteplacesapp.Location location : favouritePlacesList) {
+
+            LatLng locationLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.addCircle(new CircleOptions()
+                    .center(locationLatLng)
+                    .radius(location.getRadius())
+                    .strokeColor(getColor(R.color.favouriteMarkerStrokeColor))
+                    .fillColor(getColor(R.color.favouriteMarkerFillColor))
+                    .strokeWidth(5.0f));
+
+            Marker marker = mMap.addMarker(new MarkerOptions().position(locationLatLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_beenhere_black_24dp))
+                    .title(location.getName()));
+
+        }
+
+    }
+
+
     private boolean checkPermission() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             return true;
         } else {
             requestPermissions();
@@ -153,7 +232,8 @@ public class PlacesActivity extends FragmentActivity implements OnMapReadyCallba
         mLocationRequest = new LocationRequest()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(UPDATE_INTERVAL)
-                .setFastestInterval(FASTEST_INTERVAL);
+                .setFastestInterval(FASTEST_INTERVAL)
+                .setSmallestDisplacement(1);;
 
         // Create LocationSettingsRequest object using location request
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
@@ -182,6 +262,7 @@ public class PlacesActivity extends FragmentActivity implements OnMapReadyCallba
 
         // Update last known location
         lastLocation = location;
+        Log.d(TAG, "onLocationChanged: ");
 
         // LatLng Object for use with maps
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
@@ -190,8 +271,9 @@ public class PlacesActivity extends FragmentActivity implements OnMapReadyCallba
     }
 
     public void showCurrentLocation(LatLng currentLatLng) {
-        if(mMap == null) return;
+        if (mMap == null) return;
+
         // Center camera on current location
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15.0f));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18.0f));
     }
 }
